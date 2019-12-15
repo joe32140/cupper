4c2b8aabe2ff45e9fc31e63ea16a4f0983ddc75a
Joe <joe32140@hotmail.com.tw>
2019-12-15 16:00:18 -0700

    update

--- publish.sh
+++ publish.sh
@@ -5,7 +5,12 @@ git add -A
 git commit -m "$1"
 git push
 cp -a docs/* ../joe32140.github.io/
+cp -a docs/* ../ChaoChunHsu.github.io/
 cd ../joe32140.github.io/
+git add -A
+git commit -m "$1"
+git push
+cd ../ChaoChunHsu.github.io/
 git add -A
 git commit -m "$1"
 git push
